30325e0f515d1572e6106d7a35bfce309137ef4b
eduardolizarazu <jelo2021@gmail.com>
2022-02-27 12:10:23 -0400

    widget apilamiento textos

--- lib/review.dart
+++ lib/review.dart
@@ -2,12 +2,63 @@ import 'package:flutter/material.dart';
 
 class Review extends StatelessWidget {
   String pathImage = "assets/img/people.jpg";
-
-  Review(this.pathImage);
+  String name = "Varuna Yasas";
+  String details = "1 review 5 photos";
+  String comments = 'There is an amazing place in Sri Lanka';
+  Review(this.pathImage, this.name, this.details, this.comments);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final userComment = Container(
+      margin: EdgeInsets.only(left: 20.0),
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: const TextStyle(
+          fontFamily: 'Lato',
+          fontSize: 13.0,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    );
+
+    final userInfo = Container(
+      margin: EdgeInsets.only(left: 20.0),
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: const TextStyle(
+          fontFamily: 'Lato',
+          fontSize: 13.0,
+          color: Color(0xFFa3a5a7),
+        ),
+      ),
+    );
+
+    final userName = Container(
+      margin: EdgeInsets.only(left: 20.0),
+      child: Text(
+        name,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontFamily: 'Lato',
+          fontSize: 17.0,
+        ),
+      ),
+    );
+
+    final usarDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        // APILAMIENTO
+        userName,
+        userInfo,
+        userComment,
+      ],
+    );
+
     final photo = Container(
       margin: const EdgeInsets.only(
         top: 20.0,
@@ -21,7 +72,7 @@ class Review extends StatelessWidget {
       ),
     );
     return Row(
-      children: <Widget>[],
+      children: <Widget>[photo, userComment],
     );
   }
 }
