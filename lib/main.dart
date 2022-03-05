6821b42f017e59361037cbd51d85638e967dcfca
eduardolizarazu <jelo2021@gmail.com>
2022-03-05 10:25:28 -0400

    Finished the home part

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //PlatziTrips()
-      home: PlatziTripsCupertino(),
+      // PlatziTrips()
+      // PlatziTripsCupertino()
+      home: PlatziTrips(),
     );
   }
 }
